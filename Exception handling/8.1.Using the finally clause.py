b315adb70a294594d6b6b1609f1e65f796e04272
Drevlyanin <121987689+Drevlyanin@users.noreply.github.com>
2025-07-02 16:40:39 +0300

    8.1.Using the finally clause.py

--- Exception handling/8.1.Using the finally clause.py
+++ Exception handling/8.1.Using the finally clause.py
@@ -8,7 +8,7 @@ except ValueError:  # Catch the raised ValueError
     print("Except ValueError!")  # Print error message
     sys.exit()  # Terminate program execution
     
-finally:  # Incorrect keyword (should be 'finally')
+finally:
     print("Let's pay attention to the last details.")  # Would execute if syntax was correct
     
 print("This code will not execute.")  # Won't execute due to sys.exit()
